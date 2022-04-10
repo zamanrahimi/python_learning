7baf6ffe8f406bd1f40d8b0ddbd051a5fac7298a
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-10 15:36:06 +0500

    Six ways of creating array in Python, array, linspace, logspace, arange, zeros, ones

--- dev.py
+++ dev.py
@@ -1,4 +1,19 @@
 from numpy import * 
 
-arr = array([3,4.7])
-print (arr)
+# six ways of creating array 
+
+# array, linspace,logspace,arange,zeros,ones
+
+arr = array([3,4]) # 1. 
+lin = linspace(1,10) # 2. 
+log = logspace(1,10) # 3. 
+arn = arange(1,10) # 4. 
+ze = zeros(10) # 5. 
+on = ones(10) # 6. 
+
+print(arr)
+print(lin)
+print(log)
+print(arn)
+print(ze)
+print(on)
