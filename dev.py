f1588ee520d8c94fcbeede93910325ca4ed0eda5
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-11 16:56:16 +0500

    10, - ,Module how to import subfolder in python

--- dev.py
+++ dev.py
@@ -2,10 +2,14 @@ import sys
   
 # sys.path.insert(0, './pages')
 sys.path.append('./pages')
+sys.path.append('./pages/contact')
   
 #sub_page is sub_page.py which has add() and sub() functions
 from sub_page import *
+from phone import *
   
 # calling odd_even function
 add(3,5)
 sub(10,5)
+
+myphone()
---
+++ pages/contact/phone.py
@@ -0,0 +1,2 @@
+def myphone():
+	print("This is your phone number: 00688")
