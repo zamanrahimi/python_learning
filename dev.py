8fb45254e7f2cbe585da76cd9db11c50370f9fe2
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-11 13:16:53 +0500

    How to print factorial in a python function

--- dev.py
+++ dev.py
@@ -1,16 +1,8 @@
-def fib (n): 
+def fuct(n):
+	f = 1
+	for i in range (1, n+1):
+	
+		f = f*i
+	return f 
 
-	a=0
-	b=1
-	print (a)
-	print (b)
-
-	for i in range(2, n):
-		c = a+b 
-		a =b 
-		b = c
-
-		print (c)
-
-
-fib (10)
+print (fuct(5) )
