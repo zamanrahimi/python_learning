92fda9c020c35f63d73c82215a93bc841e1ee6bd
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-11 11:48:18 +0500

    How to pass multiple arguments with ** to a function and print all the key and values

--- dev.py
+++ dev.py
@@ -1,15 +1,7 @@
-from subprocess import Popen
-p = Popen("m.bat", cwd=r"C:\Users\test\Desktop\python tutorial")
-stdout, stderr = p.communicate()
+def list(**name):
+	
+	for k, v in name.items():
+		print(k + " - ", v)
 
 
-def multiple_parameter(a, *b):
-	c  = a 
-	for i in b: 
-		c = c + i
-
-	print (c)
-
-
-multiple_parameter(3,4,5,6,7,100)
-
+list(name='Ali', age=20, phone='4566')
