affb40ae41c9e4f9d7dda3b36baa0cb6f46b8046
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-11 17:20:40 +0500

    11, - ,How to use __naem__ variable

--- dev.py
+++ dev.py
@@ -1,15 +1,5 @@
-import sys
-  
-# sys.path.insert(0, './pages')
-sys.path.append('./pages')
-sys.path.append('./pages/contact')
-  
-#sub_page is sub_page.py which has add() and sub() functions
-from sub_page import *
-from phone import *
-  
-# calling odd_even function
-add(3,5)
-sub(10,5)
+from test import * 
 
-myphone()
+
+hello()
+
---
+++ test.py
@@ -0,0 +1,5 @@
+
+def hello():
+	print ('Hello from: '+ __name__)
+if __name__ == '__main__':
+	hello()
