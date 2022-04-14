e2ffd2ca1c1f95ec9b96ee2cce6d20f017c511ce
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-14 15:01:38 +0500

    10, - ,Python - Selection sort

--- dev.py
+++ dev.py
@@ -1,12 +1,15 @@
+# Selection Sort: compare the first or min element with each element in the list
 def sort(nums):
-	for i in range (len(nums)-1,0, -1):
-		for j in range(i):
-			if nums[j] > nums[j+1]:
-				temp = nums[j]
-				nums[j]= nums[j+1]
-				nums[j+1] = temp
+	for i in range (8):
+		minpos = i
+		for j in range(i,8):
+			if nums[j] < nums[minpos]:
+				minpos = j
+				temp = nums[i]
+				nums[i] = nums[minpos]
+				nums[minpos] = temp
 
-nums = [5,3,8,6,7,2]
+nums = [5,3,8,6,7,2,1,0]
 sort(nums)
 
 print(nums)
