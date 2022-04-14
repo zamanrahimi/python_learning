d187accf8fc9831d13ba81a88bf200d49fbcf5dd
zamanrahimi <zaman.rahimi1368@gmail.com>
2022-04-14 15:48:49 +0500

    14, - ,Python-Mysql: insert data into the table

--- dev.py
+++ dev.py
@@ -18,4 +18,11 @@ li = myc.execute("show tables")
 results = myc.fetchall()
 for r in results:
   print (r)
+  
+# --------- Insert data into the emp1 table -----------
+li = myc.execute("insert into emp1 values(2, 'Khan')")
+mycon.commit()
+
+
+
 
